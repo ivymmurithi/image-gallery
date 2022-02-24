a42bd4407362e37622e5b143cf5e58263e27edca
ivymmurithi <ivymurithi@gmail.com>
2022-02-24 20:01:16 +0300

    add image field to model

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
+    gallery_image = models.ImageField(upload_to = 'galleries/', blank=False)
+
 
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
