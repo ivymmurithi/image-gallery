afd2be372a75b711db5f67f84401f26e29f8e707
ivymmurithi <ivymurithi@gmail.com>
2022-02-24 16:18:02 +0300

    add db models

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,13 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
-   
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
