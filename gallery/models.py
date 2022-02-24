89da1911f1aec8008d07ed1bededaebb02a126b0
ivymmurithi <ivymurithi@gmail.com>
2022-02-24 21:33:31 +0300

    final model

--- gallery/models.py
+++ gallery/models.py
@@ -1,15 +1,26 @@
-from unicodedata import category
 from django.db import models
 
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
-    gallery_image = models.ImageField(upload_to = 'galleries/', blank=False)
+    gallery_image = models.ImageField(upload_to = 'galleries/')
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.name
 
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
 
+
+    def __str__(self) -> str:
+        return self.location_name
+
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
+
+
+    def __str__(self) -> str:
+        return self.category_name
