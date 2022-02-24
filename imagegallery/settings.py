4f27c72e9bbc1f92c286b7c79cd0f884ce2ca8a0
ivymmurithi <ivymurithi@gmail.com>
2022-02-24 14:29:51 +0300

    add db settings and setup and time zone

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'imagegallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imagegallery',
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD')
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
