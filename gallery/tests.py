4188510a03a33d5c9e794998db0a7a76f47daa30
ivymmurithi <ivymurithi@gmail.com>
2022-02-25 11:56:06 +0300

    delete migrations and test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Image,Category,Location
+import tempfile
 
 # Create your tests here.
+class Image(TestCase):
+    def setUp(self):
+        self.image1 = Image(name = "ivy's photo",description = "A photo of Ivy",location = 'Africa', category = 'Art')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image1,Image))
