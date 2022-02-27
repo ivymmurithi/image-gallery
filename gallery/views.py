2dabf78986dc3a0899c1b547b6b0d8de58520828
ivymmurithi <ivymurithi@gmail.com>
2022-02-27 16:45:58 +0300

    search for location and category enabled

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render
 from .models import Image
 
@@ -5,7 +6,18 @@ from .models import Image
 def hello(request):
     if request.method == 'POST':
         if 'results'in request.POST and request.POST.get("results"):
-            image_object = Image.objects.filter(category__category_name__icontains=request.POST['results'])
+            search_query = request.POST['results']
+            filter_category = {
+                "category__category_name__icontains": search_query, 
+            }
+            filter_location = {
+                "location__location_name__icontains": search_query,
+            }
+            image_object = Image.objects.filter(**filter_location)
+            if len(image_object) < 1:
+                image_object = Image.objects.filter(**filter_category)
+            if len(image_object) == 0:
+                message('Search term does not exist')
     else:
         image_object = Image.objects.all()
     return render(request, 'index.html', {"images":image_object})
